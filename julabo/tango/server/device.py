e42327e9f9083b76d186d1dc8f6b002b07374a6b
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-09-01 19:19:28 +0200

    tango: Document (desc, label and unit) attributes and commands

--- julabo/tango/server/device.py
+++ julabo/tango/server/device.py
@@ -41,82 +41,95 @@ class BaseJulabo(Device):
     async def dev_status(self):
         return await self.julabo.status()
 
-    @attribute(dtype=str)
+    @attribute(dtype=str, label="Identification")
     async def identification(self):
+        """Device identification (model and version)"""
         return await self.julabo.identification()
 
-    @attribute(dtype=bool)
+    @attribute(dtype=bool, label="Is started?")
     async def is_started(self):
+        """Is device started or not"""
         return await self.julabo.is_started()
 
     @command
     async def start(self):
+        """Start the device"""
         await self.julabo.start()
 
     @command
     async def stop(self):
+        """Stop the device"""
         await self.julabo.stop()
 
 
 class BaseJulaboCirculator(BaseJulabo):
 
-    @attribute(dtype=float)
+    @attribute(dtype=float, label="Bath temperature", unit="degC")
     async def bath_temperature(self):
+        """Actual bath temperature"""
         return await self.julabo.bath_temperature()
 
-    @attribute(dtype=float)
+    @attribute(dtype=float, label="Heating power", unit="%")
     async def heating_power(self):
+        """Heating power being used"""
         return await self.julabo.heating_power()
 
-    @attribute(dtype=float)
+    @attribute(dtype=float, label="External temperature", unit="degC")
     async def external_temperature(self):
+        """Temperature registered by external PT100 sensor"""
         return await self.julabo.external_temperature()
 
-    @attribute(dtype=float)
+    @attribute(dtype=float, label="Safety temperature", unit="degC")
     async def safety_temperature(self):
+        """Temperature registered by the safety sensor"""
         return await self.julabo.safety_temperature()
 
-    @attribute(dtype=float)
+    @attribute(dtype=float, label="Set point 1", unit="degC")
     async def set_point_1(self):
+        """Working temperature set point channel 1"""
         return await self.julabo.set_point_1()
 
     @set_point_1.setter
     async def set_point_1(self, value):
         await self.julabo.set_point_1(value)
 
-    @attribute(dtype=float)
+    @attribute(dtype=float, label="Set point 2", unit="degC")
     async def set_point_2(self):
+        """Working temperature set point channel 2"""
         return await self.julabo.set_point_2()
 
     @set_point_2.setter
     async def set_point_2(self, value):
         await self.julabo.set_point_2(value)
 
-    @attribute(dtype=float)
+    @attribute(dtype=float, label="Set point 3", unit="degC")
     async def set_point_3(self):
+        """Working temperature set point channel 3"""
         return await self.julabo.set_point_3()
 
     @set_point_3.setter
     async def set_point_3(self, value):
         await self.julabo.set_point_3(value)
 
-    @attribute(dtype=float)
+    @attribute(dtype=float, label="High temperature", unit="degC")
     async def high_temperature(self):
+        """High temperature warning limit"""
         return await self.julabo.high_temperature()
 
     @high_temperature.setter
     async def high_temperature(self, value):
         await self.julabo.high_temperature(value)
 
-    @attribute(dtype=float)
+    @attribute(dtype=float, label="Low temperature", unit="degC")
     async def low_temperature(self):
+        """Low temperature warning limit"""
         return await self.julabo.low_temperature()
 
     @low_temperature.setter
     async def low_temperature(self, value):
         await self.julabo.low_temperature(value)
 
-    @attribute(dtype=int, min_value=1, max_value=3)
+    @attribute(dtype=int, label="Active set point channel", min_value=1, max_value=3)
     async def active_set_point_channel(self):
         return await self.julabo.active_set_point_channel()
 
@@ -124,33 +137,32 @@ class BaseJulaboCirculator(BaseJulabo):
     async def active_set_point_channel(self, value):
         await self.julabo.active_set_point_channel(value)
 
-    @attribute(dtype=str)
+    @attribute(dtype=str, label="Self tunning")
     async def self_tunning(self):
+        """Self tunning (off|once|always)"""
         return (await self.julabo.self_tunning()).name
 
     @self_tunning.setter
     async def self_tunning(self, value):
         await self.julabo.self_tunning(value)
 
-    @attribute(dtype=str)
+    @attribute(dtype=str, label="External input")
     async def external_input(self):
+        """External programmer input (voltage|current)"""
         return (await self.julabo.external_input()).name
 
     @external_input.setter
     async def external_input(self, value):
         await self.julabo.external_input(value)
 
-    @attribute(dtype=str)
+    @attribute(dtype=str, label="Temperature control")
     async def temperature_control(self):
+        """Temperature control (internal|external)"""
         return (await self.julabo.temperature_control()).name
 
     @temperature_control.setter
     async def temperature_control(self, value):
         await self.julabo.temperature_control(value)
-
-    @attribute(dtype=bool)
-    async def is_started(self):
-        return await self.julabo.is_started()
 
 
 class JulaboCF(BaseJulaboCirculator):
