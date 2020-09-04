c73540f125417d69747af502ad46cccda25e778a
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-09-04 17:27:33 +0200

    Bump version: 2.0.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name="julabo",
     author="Tiago Coutinho",
     author_email="tcoutinho@cells.es",
-    version="2.0.0",
+    version="2.1.0",
     description="julabo library",
     long_description=description,
     long_description_content_type="text/markdown",
